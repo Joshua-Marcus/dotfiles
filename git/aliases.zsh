cb3718e542e3eff342d7417efe83177d80eb9648
Joshua Marcus <josh.m@resvu.com.au>
2021-10-10 23:24:14 +1030

    updated alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,7 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
@@ -22,3 +22,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias go="git remote show origin"
+alias gdo="git remote remove origin"
