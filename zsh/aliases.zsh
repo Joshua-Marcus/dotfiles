2e2ba49f073f6f35850408fa3d0c37641a171e54
Joshua-Marcus <josh.m@resvu.com.au>
2022-01-25 06:34:24 +1030

    updated files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,5 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear'
 alias kp='killport'
-alias py='python3'
 alias yys='yarn && yarn start'
 alias ycc='yarn && yarn run ngcc'
-alias rmy="rm -rf node_modules && yarn && yarn run ngcc"
